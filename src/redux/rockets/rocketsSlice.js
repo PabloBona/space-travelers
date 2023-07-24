7b4387f21866e02ea885f1b3673a5e91123eb835
Alibaba2023 <126192888+Alibaba2023@users.noreply.github.com>
2023-07-25 01:22:17 +0430

    Rockets data fetched from API

--- src/redux/rockets/rocketsSlice.js
+++ src/redux/rockets/rocketsSlice.js
@@ -1,4 +1,23 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+const formattedBooks = (data) => data.map((rocket) => ({
+  id: rocket.id,
+  rocketname: rocket.rocket_name,
+  type: rocket.rocket_type,
+  image: rocket.flickr_images[0],
+}));
+
+export const getRocketData = createAsyncThunk('rockets/getRocketData', async () => {
+  try {
+    const rocketsDataLink = 'https://api.spacexdata.com/v3/rockets';
+    const response = await fetch(rocketsDataLink);
+    const data = await response.json();
+    const formattedData = formattedBooks(data);
+    return formattedData;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+});
 
 const initialState = {
   rockets: [],
@@ -9,10 +28,23 @@ const initialState = {
 const rocketsSlice = createSlice({
   name: 'rockets',
   initialState,
-  reducers: {
-
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getRocketData.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getRocketData.fulfilled, (state, action) => {
+        state.isLoading = false;
+        if (state.rockets.length === 0) {
+          state.rockets = action.payload;
+        }
+      })
+      .addCase(getRocketData.rejected, (state) => {
+        state.isLoading = false;
+        state.hasError = true;
+      });
   },
 });
 
-export const { rocketsLoading, rocketsReceived } = rocketsSlice.actions;
 export default rocketsSlice.reducer;
