f648d9b11ec8fca7a83eff0a0d9fccb5c28c68ac
Alibaba2023 <126192888+Alibaba2023@users.noreply.github.com>
2023-07-25 15:26:02 +0430

    Rockets displayed in a ul tag

--- src/components/Rockets.jsx
+++ src/components/Rockets.jsx
@@ -1,7 +1,41 @@
-const Rockets = () => (
-  <div className="container">
-    
-  </div>
-);
+import React, { useEffect } from 'react';
+import '../style/rockets.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { getRocketData } from '../redux/rockets/rocketsSlice';
+
+function Rockets() {
+  const { rockets, isLoading, error } = useSelector((store) => store.rockets);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getRocketData());
+  }, [dispatch]);
+  if (isLoading) {
+    return <div>Loading...</div>; // Show a loader while fetching data
+  }
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+
+  return (
+    <div className="container">
+      <ul>
+        {rockets.map((rocket) => (
+          <li key={rocket.id} className="rocket-item">
+            <div className="rocket-image-container">
+              <img src={rocket.imageRocket} alt="rocket_image" className="rocket-image" key={rocket.id} />
+            </div>
+            <div className="rocket-description-container">
+              <h3 className="rocket-name">{rocket.rocketName}</h3>
+              <p className="rocket-description">{rocket.description}</p>
+              <button type="button" className="booking-btn">BOOK ROCKET</button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
 
 export default Rockets;
