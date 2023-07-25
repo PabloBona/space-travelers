164e504e172b21a6fe757b32f7b4b8f55102c000
PabloBona <pablobonasera@gmail.com>
2023-07-24 22:20:44 -0300

    Add DragonCard for render every dragon Add styles in dragon.css Remove indecx.css color to mach required desing add border bottom Refactoring Dragon.jsx

---
+++ src/components/DragonCard.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import '../style/dragons.css';
+
+const DragonCard = ({ dragon }) => (
+  <div className="dragon-card rounded my-3" key={dragon.id}>
+    <div className="row">
+      <div className="col-sm-12 col-md-5">
+        {dragon.name === 'Dragon 1' ? (
+          <img className="w-100" src={dragon.flickr_images[1]} alt={dragon.name} />
+        ) : (
+          <img className="w-100" src={dragon.flickr_images[2]} alt={dragon.name} />
+        )}
+      </div>
+      <div className="col-sm-12 col-md-7">
+        <div className="py-3">
+          <h2 className="">
+            {dragon.name}
+          </h2>
+        </div>
+        <div className="">{dragon.description}</div>
+        <div className="py-3">
+          <button type="submit" className="btn btn-primary">Reserve Dragon</button>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+DragonCard.propTypes = {
+  dragon: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    flickr_images: PropTypes.arrayOf(PropTypes.string).isRequired,
+    name: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default DragonCard;
