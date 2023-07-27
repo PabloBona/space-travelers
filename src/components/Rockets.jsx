50bf727b6b7c468a70f3627464a952452850518b
Alibaba2023 <126192888+Alibaba2023@users.noreply.github.com>
2023-07-27 18:09:15 +0430

    Rocket structure changed

--- src/components/Rockets.jsx
+++ src/components/Rockets.jsx
@@ -1,12 +1,15 @@
 import React, { useEffect } from 'react';
 import '../style/rockets.css';
 import { useSelector, useDispatch } from 'react-redux';
-import { getRocketData } from '../redux/rockets/rocketsSlice';
-import Rocket from './RocketCard';
+import { getRocketData, reserveRocket } from '../redux/rockets/rocketsSlice';
 
 function Rockets() {
   const { rockets, isLoading, error } = useSelector((store) => store.rockets);
   const dispatch = useDispatch();
+
+  const handleButtonClick = (rocketId) => {
+    dispatch(reserveRocket(rocketId));
+  };
 
   useEffect(() => {
     dispatch(getRocketData());
@@ -24,7 +27,35 @@ function Rockets() {
     <div className="container">
       <ul>
         {rockets.map((rocket) => (
-          <Rocket key={rocket.id} rocket={rocket} />
+          <li key={rocket.id} className="rocket-item">
+            <div className="rocket-image-container">
+              <img src={rocket.imageRocket} alt="rocket_image" className="rocket-image" key={rocket.id} />
+            </div>
+            <div className="rocket-description-container">
+              <h3 className="rocket-name">{rocket.rocketName}</h3>
+              <div>
+                {rocket.isReserved && <samp>Reserved </samp>}
+                <p className="rocket-description">{rocket.description}</p>
+              </div>
+              {rocket.isReserved ? (
+                <button
+                  type="button"
+                  className="cancel-btn"
+                  onClick={() => handleButtonClick(rocket.id)}
+                >
+                  Cancel Reservation
+                </button>
+              ) : (
+                <button
+                  type="button"
+                  className="booking-btn"
+                  onClick={() => handleButtonClick(rocket.id)}
+                >
+                  BOOK ROCKET
+                </button>
+              )}
+            </div>
+          </li>
         ))}
       </ul>
     </div>
