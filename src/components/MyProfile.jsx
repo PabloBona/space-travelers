f84ba6d6d1e820450024b2b15132ecf9df6c8fa7
PabloBona <pablobonasera@gmail.com>
2023-07-28 14:46:58 -0300

    Add styles

--- src/components/MyProfile.jsx
+++ src/components/MyProfile.jsx
@@ -15,27 +15,37 @@ const MyProfile = () => {
     <section className="container">
       <div className="row">
         <div className="col-lg-4 col-md-4 col-sm-12">
-          <h2 className="my-3 text-primary">My Rockets</h2>
+          <h2 className="my-3 text-primary d-flex justify-content-center">My Rockets</h2>
           <ul className="list-group custom-list">
-            {reservedRockets.map((rocket) => (
-              <li className="list-group-item" key={rocket.id}>{rocket.rocketName}</li>
-            ))}
+            {reservedRockets.length > 0 ? (
+              reservedRockets.map((rocket) => (
+                <li className="list-group-item" key={rocket.id}>{rocket.rocketName}</li>
+              ))
+            ) : (
+              <li className="list-group-item d-flex justify-content-center">No Rockets reserved</li>
+            )}
           </ul>
         </div>
         <div className="col-lg-4 col-md-4 col-sm-12">
-          <h2 className="my-3 text-primary">My Missions</h2>
+          <h2 className="my-3 text-primary d-flex justify-content-center">My Missions</h2>
           <ul className="list-group custom-list">
-            {reservedMissions.map(({ id, name }) => (
-              <li className="list-group-item" key={id}>{name}</li>
-            ))}
+            {reservedMissions.length > 0 ? (
+              reservedMissions.map(({ id, name }) => (
+                <li className="list-group-item" key={id}>{name}</li>
+              ))) : (
+                <li className="list-group-item d-flex justify-content-center">No Missions reserved</li>
+            )}
           </ul>
         </div>
         <div className="col-lg-4 col-md-4 col-sm-12">
-          <h2 className="my-3 text-primary">My Dragons</h2>
+          <h2 className="my-3 text-primary d-flex justify-content-center">My Dragons</h2>
           <ul className="list-group custom-list">
-            {reservedDragons.map((dragon) => (
-              <li className="list-group-item" key={dragon.id}>{dragon.name}</li>
-            ))}
+            {reservedDragons.length > 0 ? (
+              reservedDragons.map((dragon) => (
+                <li className="list-group-item" key={dragon.id}>{dragon.name}</li>
+              ))) : (
+                <li className="list-group-item d-flex justify-content-center">No Dragons reserved</li>
+            )}
           </ul>
         </div>
       </div>
